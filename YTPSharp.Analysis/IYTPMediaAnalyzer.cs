63d3a837637f70d8a6c082e0d7f44d7e38f173b8
JD Pierce <brkfstmnchr@gmail.com>
2020-09-28 21:30:29 +0700

    initial commit; began planning project/inheritence/interface hierarchy
    initial commit; began planning project/inheritence/interface hierarchy

---
+++ YTPSharp.Analysis/IYTPMediaAnalyzer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using YTPSharp.Media;
+
+namespace YTPSharp.Analysis
+{
+    interface IYTPMediaAnalyzer
+    {
+        YTPMediaAnalysis Analyzie(IYTPMedia media);
+    }
+}
---
+++ YTPSharp.Image/YTPAudio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using YTPSharp.Media;
+
+namespace YTPSharp.Audio
+{
+    public class YTPAudio : IYTPMedia
+    {
+    }
+}
